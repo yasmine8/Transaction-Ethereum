2b5e1df9162bf31fc1f47a3279e6a55013cd452f
yasmine <yasmine159357@hotmail.fr>
2023-04-19 15:39:17 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import {Footer, Loader, Navbar, Services, Transactions, Welcome} from './components'
+
+const App = () =>  {
+
+  return (
+    <div className="min-h-screen">
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from '../../../smart_contract/artifacts/contracts/Transactions.sol/Transactions.json';
+
+
+export const contractABI = abi.abi;
+export const contractAddress ='0x5FbDB2315678afecb367f032d93F642f64180aa3';
